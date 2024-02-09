930d7e16ad04beca700580acd7b07bdbbbb5aadf
Wallcroft-Ubuntu <wallcroft.uk.sas@gmail.com>
2024-02-09 04:03:14 +0100

    fix

--- vanosilla-linux/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(vanosilla):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="vanosilla" command="/usr/bin/vanosilla"
--- vanosilla-linux/debian/vanosilla.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the vanosilla package
-#
-0 4	* * *	root	[ -x /usr/bin/vanosilla_maintenance ] && /usr/bin/vanosilla_maintenance
